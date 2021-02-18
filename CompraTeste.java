af1d8bbc58cc927cd76e4f3d26ae8707cceb6dad
Tiago <74260711+7iago@users.noreply.github.com>
2021-02-18 09:11:44 -0300

    teste

--- CompraTeste.java
+++ CompraTeste.java
@@ -9,7 +9,7 @@ public class CompraTeste {
 		compra1.Cliente = "Joao Pedro";
 		compra1.adicionarItem(new Item("Caneta", 20, 7.45));
 		compra1.adicionarItem(new Item("Borracha", 12, 3.89));
-		compra1.adicionarItem(new Item("Caderno", 3, 18.79));
+		compra1.adicionarItem(new Item("Lapis", 3, 18.79));
 		
 		System.out.println("Quantidade total de itens: " + compra1.itens.size());
 		
